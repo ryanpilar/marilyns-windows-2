02743ffac32676195cd4d0bb91768fd53248bee0
ryanpilar <ryan_pilar@outlook.com>
2022-11-06 13:30:23 -0700

    added 404 to routes

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -8,6 +8,8 @@ import BlogPost from './Segments/BlogPost';
 import Blogs from './Pages/Blogs'
 import Gallery22 from './Pages/Gallery22';
 
+import NotFound from './Pages/NotFound';
+
 class Components extends React.Component {
     render() {
         return (
@@ -23,8 +25,12 @@ class Components extends React.Component {
                             <Route path='/post/:id' exact component={BlogPost} />
                             <Route path='/services' exact component={Services} />
 
-                            <Route component={Error} />
-                            <Route component={Error} />
+                            <Route path='*'>
+                                <NotFound />
+                            </Route>
+
+                            {/* <Route component={Error} />
+                            <Route component={Error} /> */}
                             
                         </Switch>
 
