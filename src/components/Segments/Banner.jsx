714c6c867e899fed5b03ede9de78c3467e93654d
ryanpilar <ryan_pilar@outlook.com>
2023-02-16 14:44:35 -0700

    test background-attachment

--- src/components/Segments/Banner.jsx
+++ src/components/Segments/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = ({ bgimage, title, posLeft, posRight }) => {
   return (
     <>
       <div
-        className="wt-bnr-inr overlay-wraper bg-parallax bg-top-center "
+        className="wt-bnr-inr overlay-wraper bg-parallax bg-parallax-fallback bg-top-center"
         
         data-stellar-background-ratio="0.5"
         style={{ backgroundImage: "url(" + bgimage + ")" }}
