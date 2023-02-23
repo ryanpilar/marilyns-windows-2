4bf56a28a6ba4c0912521dd62204966b99e56825
ryanpilar <ryan_pilar@outlook.com>
2023-02-23 02:27:25 -0700

    added google analytics snippets test

--- src/index.js
+++ src/index.js
@@ -18,15 +18,15 @@ ReactDOM.render(
   </HelmetProvider>,document.getElementById('root')
 );
 
-const SendAnalytics = ()=> {
-  GA4React.send({
-    hitType: "pageview",
-    page: window.location.pathname,
-  });
-}
+// const SendAnalytics = ()=> {
+//   GA4React.send({
+//     hitType: "pageview",
+//     page: window.location.pathname,
+//   });
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-reportWebVitals(SendAnalytics);
+reportWebVitals();
