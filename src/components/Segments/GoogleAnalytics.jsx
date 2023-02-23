7a5ea00a8eb5d99fe69aacfb81f1f3ba691334c8
ryanpilar <ryan_pilar@outlook.com>
2023-02-23 01:10:48 -0700

    added google analytics snippets test

---
+++ src/components/Segments/GoogleAnalytics.jsx
@@ -0,0 +1,18 @@
+import { useLocation } from "react-router-dom";
+import { GA4React } from "react-ga4";
+
+const GoogleAnalytics = () => {
+
+  useEffect(() => {
+
+    GA4React.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_MEASUREMENT_ID);
+
+    const location = useLocation();
+
+    GA4React.send({ hitType: "pageview", page: location.pathname });
+
+  }, []);
+
+};
+
+export default GoogleAnalytics;
