bbfe8979674fcadfe34e0017b6588edf17a413e4
ryanpilar <ryan_pilar@outlook.com>
2023-04-16 01:49:10 -0600

    updated sitemap problem

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -137,6 +137,7 @@ async function generateSitemap () {
 
   // Add each gallery post to the sitemap
   galleryPosts.forEach((galleryPost) => {
+    console.log('galleryPost', galleryPost);
     smStream.write({
       url: `/gallery/room/${galleryPost.slug}`,
       changefreq: "monthly",
